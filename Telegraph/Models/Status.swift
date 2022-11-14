c8920bcb03c809b451718c1fccf552edaa198267
soroushamdg <soroushmpour@gmail.com>
2022-11-14 14:33:12 -0500

    feat: status

---
+++ Telegraph/Models/Status.swift
@@ -0,0 +1,17 @@
+//
+//  Status.swift
+//  Telegraph
+//
+//  Created by Soro on 2022-11-14.
+//
+
+import Foundation
+
+enum Status: String {
+    case Available = "Available"
+case Busy = "Busy"
+case AtSchool = "At school"
+case AtTheMovies = "At the movies"
+case AtWork = "AtWork"
+
+}
