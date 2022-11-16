93b2cdfd609f71e2febd795e8f578f8edac48f15
soroushamdg <soroushmpour@gmail.com>
2022-11-16 11:47:28 -0500

    feat: profile view storyboard

---
+++ Telegraph/Views/ProfileTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileTableViewController.swift
+//  Telegraph
+//
+//  Created by Soro on 2022-11-16.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+    
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    //MARK: LIFE CYCLE
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+}
