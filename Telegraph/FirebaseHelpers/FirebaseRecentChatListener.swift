71e81660e8adf7903792ee59e69b2c330b24418a
soroushamdg <soroushmpour@gmail.com>
2022-11-21 09:59:44 -0500

    feat: download recent chats

--- Telegraph/FirebaseHelpers/FirebaseRecentChatListener.swift
+++ Telegraph/FirebaseHelpers/FirebaseRecentChatListener.swift
@@ -13,6 +13,29 @@ class FirebaseRecentListener {
     
     private init() {}
     
+    func downloadRecentChatsFromFirestore(completion : @escaping (_ allRecents:[RecentChat]) -> Void){
+        FirebaseRefrence(.Recent).whereField(kSENDERID, isEqualTo: User.currentId).addSnapshotListener {snapshot, error in
+            var recentChats: [RecentChat] = []
+            guard let documents = snapshot?.documents else {
+                print("no documents for recent chats")
+                return
+            }
+            
+            let allRecents = documents.compactMap { (queryDocumentSnapshot) -> RecentChat? in
+                return try? queryDocumentSnapshot.data(as: RecentChat.self)
+            }
+            
+            for recent in allRecents {
+                if recent.lastMessage != "" {
+                    recentChats.append(recent)
+                }
+            }
+            
+            recentChats.sorted { $0.date! > $1.date! }
+            completion(recentChats)
+        }
+    }
+    
     func addRecent(_ recent: RecentChat) {
         do{
             try FirebaseRefrence(.Recent).document(recent.id).setData(from: recent)
@@ -20,4 +43,5 @@ class FirebaseRecentListener {
             print("Error saving recent chat ",error.localizedDescription)
         }
     }
+    
 }
