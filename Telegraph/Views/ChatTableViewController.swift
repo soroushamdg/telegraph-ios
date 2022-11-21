831090ca2a5f3e08b650acd62936c2ec1edd0b57
soroushamdg <soroushmpour@gmail.com>
2022-11-21 10:11:58 -0500

    feat: show recents in table view with search compatibility

---
+++ Telegraph/Views/ChatTableViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ChatTableViewController.swift
+//  Telegraph
+//
+//  Created by Soro on 2022-11-21.
+//
+
+import UIKit
+
+class ChatTableViewController: UITableViewController {
+    //MARK: VARS
+    var allRecents:[RecentChat] = []
+    var filteredRecents:[RecentChat] = []
+    
+    let searchController = UISearchController(searchResultsController: nil)
+
+    //MARK: VIEW LIFE CYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+        downloadRecentChats()
+        setupSearchController()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return allRecents.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! RecentChatTableViewCell
+        cell.configure(recent: allRecents[indexPath.row])
+        return cell
+    }
+    
+    //MARK: SETUP SEARCH CONTROLLER
+    private func setupSearchController(){
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = true
+        
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search user"
+        
+        searchController.searchResultsUpdater = self
+        definesPresentationContext = true
+    }
+    
+    private func filteredContentForSearchText(_ searchText: String){
+        filteredRecents = allRecents.filter { recent in
+            return recent.receiverName.lowercased().contains(searchText.lowercased())
+        }
+        tableView.reloadData()
+    }
+    
+    //MARK: Download Chats
+    private func downloadRecentChats() {
+        FirebaseRecentListener.shared.downloadRecentChatsFromFirestore { recents in
+            self.allRecents = recents
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+
+}
+
+extension ChatTableViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filteredContentForSearchText(searchController.searchBar.text!)
+    }
+}
