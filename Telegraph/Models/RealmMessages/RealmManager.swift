d49962251f7d38109350dbd8353ebde428179023
soroushamdg <soroushmpour@gmail.com>
2022-11-28 11:51:03 -0500

    feat: realm manager object defined

---
+++ Telegraph/Models/RealmMessages/RealmManager.swift
@@ -0,0 +1,27 @@
+//
+//  RealmManager.swift
+//  Telegraph
+//
+//  Created by Soro on 2022-11-28.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager{
+    static let shared = RealmManager()
+    
+    let realm = try! Realm()
+    
+    private init(){}
+    
+    func saveToRealm<T: Object>(_ object: T) {
+        do {
+            try realm.write({
+                realm.add(object, update: .all)
+            })
+        } catch {
+            print("Error saving realm object.", error.localizedDescription)
+        }
+    }
+}
